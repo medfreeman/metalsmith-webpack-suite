219f4cfa8b100c0e8f9f588c30a9773fada55508
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-12-21 12:08:20 +0100

    chore: replace `require` by `import`

--- src/assets/js/page.js
+++ src/assets/js/page.js
@@ -1,4 +1,4 @@
-require('../css/page.css')
+import '../css/page.css'
 
 document.addEventListener('DOMContentLoaded', function(event) {
   console.log('The page is loaded and ready to perform JS actions.')
